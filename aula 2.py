f65dd909f9cb3186005df0957defea3ff1f9a668
vinic8 <labsfiap@fiap.com.br>
2026-03-16 09:55:04 -0300

    aula- variaveis

---
+++ aula 2.py
@@ -0,0 +1,15 @@
+#desafio 1
+nome = input("qual seu nome:")
+print ("seja bem vindo", nome)
+
+#desafio 2
+dia = int(input("que dia voce nasceu?"))
+mes = int(input("que mes voce nasceu?"))
+ano = int(input("que ano voce nasceu?"))
+print(f"{dia}/{mes}/{ano}")
+
+#desafio 3
+num1 = float(input("digite um numero"))
+num2 = float(input("digite outro numero"))
+soma = num1 + num2
+print("sua soma e de:", soma)
