b573872a6d50bffaf160531c6969bd1e370c21ba
vinic8 <vinic.fiap@gmail.com>
2026-08-10 10:10:16 -0300

    aula 7

---
+++ Aula 07/parte 2 tupla e dict.py
@@ -0,0 +1,15 @@
+# def count_email(email):
+#     d = dict()
+#     for c in s:
+#         if c not in d:
+#             d[c] = 1
+#         else:
+#             d[c] += 1
+#     return d
+
+emails = input("digite os emails separados por virgula:")
+
+emails = emails.split('@')
+print(emails)
+
+
---
+++ Aula 07/tuplas e dict.py
@@ -0,0 +1,22 @@
+eng2sp = dict()
+print(eng2sp)
+
+eng2sp['one'] = 'uno'
+print(eng2sp)
+
+eng2sp = {
+    'one': 'uno',
+    'two': 'dos',
+    'three': 'tres',
+}
+
+print(eng2sp)
+print(eng2sp['two'])
+print(len(eng2sp))
+
+#op IN
+
+print('uno' in eng2sp)
+
+values_dict = eng2sp.values()
+print(values_dict)
